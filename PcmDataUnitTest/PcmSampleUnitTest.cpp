2e73b4dd0ac6a4e87d57deadc3703ed46e4ef899
Koji Yano <cozzyy2002@gmail.com>
2022-03-21 22:13:28 +0900

    Add test for std::string operator of IPcmSample::Value class. Add test for methods of IPcmSample that returns constant value.

--- PcmDataUnitTest/PcmSampleUnitTest.cpp
+++ PcmDataUnitTest/PcmSampleUnitTest.cpp
@@ -1,5 +1,6 @@
 #include "PcmData/PcmSample.h"
 #include "PcmData/INT24.h"
+#include "PcmData/PcmDataImpl.h"
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
@@ -8,8 +9,6 @@
 #include <memory>
 
 using namespace ::testing;
-
-using AllSampleDataTypes = Types<UINT8, INT16, INT24, float>;
 
 template<typename T>
 struct TestSample
@@ -27,7 +26,7 @@ template<> const size_t TestSample<INT16>::SamplesCount = ARRAYSIZE(Samples);
 template<> const INT24 TestSample<INT24>::Samples[] = { 0xa00000, 0 , 0x600000, 0xffffff };
 template<> const size_t TestSample<INT24>::SamplesCount = ARRAYSIZE(Samples);
 
-template<> const float TestSample<float>::Samples[] = {  -0.8f, 0 , 0.8f };
+template<> const float TestSample<float>::Samples[] = {  -1.0f, -0.8f, 0 , 0.8f, 1.0f };
 template<> const size_t TestSample<float>::SamplesCount = ARRAYSIZE(Samples);
 
 template<typename T>
@@ -40,17 +39,25 @@ public:
 	size_t testSamplesCount;
 
 	void SetUp() {
+		// Copy TestSample to buffer of type T.
 		testSamples = TestSample<T>::Samples;
 		testSamplesCount = TestSample<T>::SamplesCount;
 		buffer = std::make_unique<T[]>(testSamplesCount);
 		for(size_t i = 0; i < testSamplesCount; i++) { buffer[i] = (T)testSamples[i]; }
+
+		// Create IPcmSample object using the buffer.
 		testee.reset(createPcmSample<T>(buffer.get(), testSamplesCount * sizeof(T)));
 		ASSERT_THAT(testee.get(), Not(nullptr));
 	}
 };
 
+using AllSampleDataTypes = Types<UINT8, INT16, INT24, float>;
 TYPED_TEST_SUITE(PcmSampleTypedTest, AllSampleDataTypes);
 
+// Test for IPcmSample::isValue(index).
+// The method should return:
+//   true if index = 0 ~ (sample count - 1)
+//   false otherwise
 TYPED_TEST(PcmSampleTypedTest, isValid)
 {
 	EXPECT_TRUE(this->testee->isValid(0));
@@ -58,11 +65,14 @@ TYPED_TEST(PcmSampleTypedTest, isValid)
 	EXPECT_FALSE(this->testee->isValid(this->testSamplesCount));
 }
 
-TYPED_TEST(PcmSampleTypedTest, getValue)
+// Test for INT32/double operator of IPcmSample::Value class.
+// The operator should return value as same as sample data passed to createPcmSample() functio.
+TYPED_TEST(PcmSampleTypedTest, to_int_float)
 {
 	for(size_t i = 0; i < this->testSamplesCount; i++) {
 		auto value = (*this->testee)[i];
-		switch(this->testee->getFormatTag()) {
+		auto formatTag = this->testee->getFormatTag();
+		switch(formatTag) {
 		case WAVE_FORMAT_PCM:
 			EXPECT_EQ((INT32)value, this->testSamples[i]);
 			break;
@@ -70,8 +80,52 @@ TYPED_TEST(PcmSampleTypedTest, getValue)
 			EXPECT_EQ((double)value, this->testSamples[i]);
 			break;
 		default:
-			FAIL() << "Unknown FormatTag: " << this->testee->getFormatTag();
+			FAIL() << "Unknown FormatTag: " << formatTag;
 			break;
 		}
+	}
+}
+
+// Test for std::string operator of IPcmSample::Value class.
+// The operator should return string that is parsed to the value as same as sample data passed to createPcmSample() functio.
+TYPED_TEST(PcmSampleTypedTest, to_string)
+{
+	for(size_t i = 0; i < this->testSamplesCount; i++) {
+		auto value = (*this->testee)[i];
+		auto str = (std::string)value;
+		auto formatTag = this->testee->getFormatTag();
+		switch(formatTag) {
+		case WAVE_FORMAT_PCM:
+			EXPECT_EQ((TypeParam)atoi(str.c_str()), this->testSamples[i]);
+			break;
+		case WAVE_FORMAT_IEEE_FLOAT:
+			EXPECT_EQ((TypeParam)atof(str.c_str()), this->testSamples[i]);
+			break;
+		default:
+			FAIL() << "Unknown FormatTag: " << formatTag;
+			break;
+		}
+	}
+}
+
+// Test for methods of IPcmSample class that returns constant value.
+// The methods should return value as same as value of PcmData<T> class.
+TYPED_TEST(PcmSampleTypedTest, constants)
+{
+	auto formatTag = this->testee->getFormatTag();
+	switch(formatTag) {
+	case WAVE_FORMAT_PCM:
+		EXPECT_EQ((INT32)this->testee->getHighValue(), PcmData<TypeParam>::HighValue);
+		EXPECT_EQ((INT32)this->testee->getZeroValue(), PcmData<TypeParam>::ZeroValue);
+		EXPECT_EQ((INT32)this->testee->getLowValue(), PcmData<TypeParam>::LowValue);
+		break;
+	case WAVE_FORMAT_IEEE_FLOAT:
+		EXPECT_EQ((double)this->testee->getHighValue(), PcmData<TypeParam>::HighValue);
+		EXPECT_EQ((double)this->testee->getZeroValue(), PcmData<TypeParam>::ZeroValue);
+		EXPECT_EQ((double)this->testee->getLowValue(), PcmData<TypeParam>::LowValue);
+		break;
+	default:
+		FAIL() << "Unknown FormatTag: " << formatTag;
+		break;
 	}
 }

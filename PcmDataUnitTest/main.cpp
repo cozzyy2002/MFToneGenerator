3b5ae79b09fcbdaa125ba8b45592525455f79cb8
Koji Yano <cozzyy2002@gmail.com>
2022-03-27 12:41:44 +0900

    Move main() function from PcmDataUnitTest.cpp to main.cpp

---
+++ PcmDataUnitTest/main.cpp
@@ -0,0 +1,11 @@
+// PcmDataUnitTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	::testing::InitGoogleMock(&argc, argv);
+	return RUN_ALL_TESTS();
+}

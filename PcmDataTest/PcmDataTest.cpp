bbed35ac30f41d13420647950f40a14ba26a1906
Koji Yano <cozzyy2002@gmail.com>
2022-06-03 19:55:49 +0900

    Add -i option of PcmDataTest to output value retrieved by Value::toInt32() method.

--- PcmDataTest/PcmDataTest.cpp
+++ PcmDataTest/PcmDataTest.cpp
@@ -17,6 +17,7 @@ int main(int argc, char* argv[])
 	WORD key = 440;
 	float level = 1.0f;
 	float phaseShift = 0;
+	bool int32Value = false;
 
 	auto& sampleDataTypeProperties(PcmDataEnumerator::getSampleDatatypeProperties());
 	auto& waveFormProperties(PcmDataEnumerator::getWaveFormProperties());
@@ -31,6 +32,7 @@ int main(int argc, char* argv[])
 		if(sscanf_s(argv[i], "key=%d", &iVal) == 1) { key = iVal; continue; }
 		if(sscanf_s(argv[i], "lvl=%f", &fVal) == 1) { level = fVal; continue; }
 		if(sscanf_s(argv[i], "sft=%f", &fVal) == 1) { phaseShift = fVal; continue; }
+		if(_strcmpi(argv[i], "-i") == 0) { int32Value = true; continue; }
 
 		std::cerr << "Usage: PcmDataTest [duty=Duty] [peak=PeakPosition] [sps=SamplesPerSecond] [ch=Channels] [key=Key] [lvl=Level] [sft=PhaseSift]\n";
 		return 0;
@@ -44,7 +46,24 @@ int main(int argc, char* argv[])
 		<< "\nKey," << key
 		<< "\nLevel," << level
 		<< "\nPhase Shift," << phaseShift
+		<< "\nValue, Retrieved by " << (int32Value ? "getInt32()" : "cast operator (std::string)")
 		<< "\n\n";
+
+	// Function to convert Value to string
+	using ToString = std::string(*)(const IPcmSample::Value&);
+	auto tostring = int32Value ? (ToString)
+		// Converts value retrieved by Value::getInt32() method.
+		[](const IPcmSample::Value& value)
+		{
+			char str[30] = "";
+			auto ret = _itoa_s(value.getInt32(), str, 10);
+			return std::string((ret == 0) ? str : "???");
+		} :
+		// Converts using cast operator (std::string) of Value.
+		[](const IPcmSample::Value& value)
+		{
+			return (std::string)value;
+		};
 
 	// Create PcmData objects and it's Handler objects associate with all combination of SampleDataType and WaveForm.
 	std::vector<std::unique_ptr<IPcmSample>> pcmSamples;
@@ -81,9 +100,9 @@ int main(int argc, char* argv[])
 			<< "," << pcmData->getBlockAlign()
 			<< "," << pcmData->getSamplesPerCycle()
 			<< "," << pcmData->getSampleBufferSize(0)
-			<< "," << (std::string)IPcmSample::getHighValue(pcmData->getSampleDataType())
-			<< "," << (std::string)IPcmSample::getZeroValue(pcmData->getSampleDataType())
-			<< "," << (std::string)IPcmSample::getLowValue(pcmData->getSampleDataType())
+			<< "," << tostring(IPcmSample::getHighValue(pcmData->getSampleDataType()))
+			<< "," << tostring(IPcmSample::getZeroValue(pcmData->getSampleDataType()))
+			<< "," << tostring(IPcmSample::getLowValue(pcmData->getSampleDataType()))
 			<< std::endl;
 	}
 	std::cout << std::endl;
@@ -103,11 +122,12 @@ int main(int argc, char* argv[])
 		}
 	}
 	std::cout << std::endl;
+
 	for(size_t pos = 0; pos < sampleCountInCycle; pos += channels) {
 		std::cout << (pos / channels);
 		for(auto& pcmSample : pcmSamples) {
 			for(WORD ch = 0; ch < channels; ch++) {
-				std::cout << "," << (std::string)(*pcmSample)[pos + ch];
+				std::cout << "," << tostring((*pcmSample)[pos + ch]);
 			}
 		}
 		std::cout << std::endl;
